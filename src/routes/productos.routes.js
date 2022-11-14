108234c5aa2ee47be17b17a6ff815c92b940921a
Lopez Nazarena <nazarenalopez40@gmail.com>
2022-11-14 18:53:21 -0300

    agregamos funcion para crear producto

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { listaProductos, obtenerProductos } from "../controllers/productos.controllers";
+import { crearProducto, listaProductos, obtenerProductos } from "../controllers/productos.controllers";
 
 const router = Router();
 
-router.route('/productos').get(listaProductos);
+router.route('/productos').get(listaProductos).post(crearProducto)
 
 
 router.route('/productos/:id').get(obtenerProductos);
+
+
+export default router
