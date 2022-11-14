f76960de7f22dddc2a8b0a807d0183ca61c6657e
Javier Uncos <javiersrfc30@gmail.com>
2022-11-14 20:41:45 -0300

    agregamos la peticion para editar producto

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { crearProducto, listaProductos, obtenerProductos } from "../controllers/productos.controllers";
+import { crearProducto, editarProducto, listaProductos, obtenerProductos } from "../controllers/productos.controllers";
 
 const router = Router();
 
 router.route('/productos').get(listaProductos).post(crearProducto)
 
 
-router.route('/productos/:id').get(obtenerProductos);
+router.route('/productos/:id').get(obtenerProductos).put(editarProducto);
 
 
 export default router
