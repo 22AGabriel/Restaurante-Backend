342647737b03e7307ae20f9525e623cd734b3600
Victoria Palacio Fernandez <victoriapalaciof@gmail.com>
2022-11-16 18:45:06 -0300

    agregue las validaciones correspondientes a la peticion put de usuarios

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -59,6 +59,12 @@ export const borrarUsuario = async (req, res) =>{
 }
 export const editarUsuarios = async(req,res)=>{
   try{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+      return res.status(400).json({
+        errores: errors.array()
+      })
+    }
     await Usuario.findByIdAndUpdate(req.params.id, req.body);
     res.status(200).json({
       mensaje:'el usuario se edito correctamente'
