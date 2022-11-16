8b49ec1f7a94699f79ba70654df4d478d2384076
Jochyd <mail>
2022-11-16 18:52:03 -0300

    agregue validationResult a editar productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -51,6 +51,12 @@ export const crearProducto = async (req, res) => {
 export const editarProducto = async (req,res)=>{
       
   try{
+    const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                errores: errors.array()
+            })
+        }
     await Producto.findByIdAndUpdate(req.params.id,req.body)
     res.status(200).json({
       mensaje:"el producto pudo ser editaddo correctamente"
