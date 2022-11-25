39f8bec1009d482b5ed10f1cdc2f93e9ca8980fa
Jochyd <mail>
2022-11-25 15:34:46 -0300

    agregue validaciones al login

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { login } from "../controllers/login.controllers";
+import { check } from "express-validator";
 
 const router = Router();
-router.route('/login').post(login);
+router.route('/login').post(
+    [
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password','El password debe contener 8 caracteres como minimo').isLength({min: 8}),
+    ],login);
 
 export default router;
