7988340ad6b945998021a42b11a38c633a7cb2d3
Gabriel Azubel <gabrielazubel63t@gmail.com>
2022-11-27 22:47:29 -0300

    contecté backend con mongoatlas

--- src/database.js
+++ src/database.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
-const url = 'mongodb://127.0.0.1:27017/sham'
+// Base de datos local
+// const url = 'mongodb://127.0.0.1:27017/sham'
+
+// Base de datos en desarrollo
+const url = 'mongodb+srv://ShamRestaurante:Sham2022@cluster0.bi2e2br.mongodb.net/restauranteSham'
 
 mongoose.connect(url);
 
